0bd83f2111f3fabe28199a8a2d59da317be60dc3
MariannaBazzani <merionbaz@gmail.com>
2023-11-28 17:14:23 +0100

    Aggiungere i file di progetto.

---
+++ Es 24.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato quadrato = new Quadrato();
+            Console.WriteLine("Inserisci il lato del quadrato: ");
+            string line = Console.ReadLine();
+            while (!float.TryParse(line, out float l)) 
+            {
+                Console.WriteLine("Valore nullo, inserire nuovamente: ");
+                line = Console.ReadLine();
+            }
+            quadrato.Lato = float.Parse(line);
+            quadrato.CalcolaSuperfice();
+        }
+    }
+}
+
---
+++ Es 24.1/Quadrato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_24._1
+{
+    internal class Quadrato
+    {
+        public float Lato { get; set; }
+        public void CalcolaSuperfice() {
+            Console.WriteLine("La superfice e': " + Lato*Lato);
+        }
+    }
+}
